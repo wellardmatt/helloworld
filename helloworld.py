fa0e91bbf028b5afafc8a6d58f58a4e564855e64
Matthew <wellardmatt@gmail.com>
2024-03-05 17:43:11 +0000

    Used a while loop to allow multiple print statements

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,6 @@
-user_input = input("Please input your print statement: ")
-print(user_input)
+while True:
+    user_input = input("Please input your print statement: ")
+    print(user_input)
+    printmore=input("Would you like to print another statement? Y/N ").upper()
+    if printmore=="N":
+        exit()
